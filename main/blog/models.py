e495d194a7a391bcb8ad37d7ea1e23a7f815646f
feichtwanger <feichtwanger7@gmail.com>
2024-06-19 00:53:59 +0500

    blog + css

--- main/blog/models.py
+++ main/blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 
 class Blog(models.Model):
@@ -10,3 +13,7 @@ class Blog(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.title
--- main/blog/views.py
+++ main/blog/views.py
@@ -4,6 +4,6 @@ from .models import Blog
 # Create your views here.
 def blog(request):
     context = {
-        'posts': Blog.objects.all()
+        'posts': Blog.objects.all().order_by('-created_at')
     }
     return render(request, 'blog/blog.html', context)
